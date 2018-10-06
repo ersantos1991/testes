03bbe2f1085b125101452f98857b2f09a2a1d9cb
unknown <ersantos.geral@gmail.com>
2018-10-06 20:52:44 +0100

    first commit

---
+++ controllers/helloCont.js
@@ -0,0 +1,12 @@
+﻿var 
+
+
+
+module.exports.hello = function (req, res) {
+    res.sendFile('hello.html', { root: 'views' })
+};
+
+
+module.exports.hello2 = function (req, res) {
+    res.send('this is my frist test');
+};
---
+++ routes/helloroute.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var router = express.Router();
+var hello_controller = require('../controllers/helloCont');
+var hello_controller = require('../controllers/hello2Cont');
+
+router.get('/', hello_controller.hello);
+
+router.get('/hellohellodois', hello_controller.hello2);
+
+module.exports = router;
